8939a6efb0bfdf01e6807c32f5388e713494833f
mrsyasysyifa26@gmail.com <mrsyasysyifa26@gmail.com>
2026-09-07 16:01:41 +0700

    praktikum-2: Komentar Kode A.3.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -6,6 +6,14 @@ public class HelloWorld {
         System.out.print("Marsya");
         System.out.println();
         System.out.println("Ini baris baru");
+
+        // Program menampilkan biodata sederhana
+        System.out.println("Nama: Marsya");
+        /* Baris di bawah ini
+        mencetak NIM mahasiswa */
+        System.out.println("NIM: 2025573010076");
+
+
         }
 }
 
