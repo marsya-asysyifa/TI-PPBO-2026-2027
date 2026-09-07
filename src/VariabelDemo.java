cce009d14b86dfc07c0f19da8e167cdd2b18e06e
mrsyasysyifa26@gmail.com <mrsyasysyifa26@gmail.com>
2026-09-07 16:11:21 +0700

    praktikum-2: Deklarasi Variabel dan Tipe Data Primitif B.1.

---
+++ src/VariabelDemo.java
@@ -0,0 +1,15 @@
+public class VariabelDemo {
+    public static void main(String[] args) {
+            String nama = "Marsya";
+            int umur = 19;
+            double tinggi = 153.5;
+            char golDarah = 'O';
+            boolean mahasiswaAktif = true;
+
+            System.out.println("Nama: " + nama);
+            System.out.println("Umur: " + umur);
+            System.out.println("Tinggi: " + tinggi);
+            System.out.println("Golongan Darah: " + golDarah);
+            System.out.println("Aktif: " + mahasiswaAktif);
+            }
+}
