6d92b6bf2b21b41fc904dba28c34af9aa9515fca
mrsyasysyifa26@gmail.com <mrsyasysyifa26@gmail.com>
2026-09-18 22:59:46 +0700

    Praktikum-2: Struktur dasar program java A.2.

--- Praktikum-2/src/HelloWorld.java
+++ Praktikum-2/src/HelloWorld.java
@@ -1,6 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Marsya Asysyifa");
+        System.out.print("Nama: ");
+        System.out.print("Marsya");
+        System.out.println();
+        System.out.println("Ini baris baru");
+
+
 
 
     }
